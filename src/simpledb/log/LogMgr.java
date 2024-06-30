f4728154b34573b38db0dc90ea79f850e78d605a
Takashi-Iskw <takashi.ishikawa.24@gmail.com>
2024-06-30 15:01:57 +0900

    add LogMgr, LogTest

--- src/simpledb/log/LogMgr.java
+++ src/simpledb/log/LogMgr.java
@@ -20,54 +20,53 @@ public class LogMgr {
         byte[] b = new byte[fm.blockSize()];
         logpage = new Page(b);
         int logsize = fm.length(logfile);
-        if(logsize == 0) {
+        if (logsize == 0) {
             currentblk = appendNewBlock();
         } else {
             currentblk = new BlockId(logfile, logsize - 1);
             fm.read(currentblk, logpage); // バッファ"logPage"にfmの内容を書き込み
         }
-        // logPageはここで新規に作成しているようだが、readしても真っ白なのでは？ 初期化みたいなものか
-        // ↑違う。逆で、fmの内容をlogpageに書き出している。それがread
+    }
+    // logPageはここで新規に作成しているようだが、readしても真っ白なのでは？ 初期化みたいなものか
+    // ↑違う。逆で、fmの内容をlogpageに書き出している。それがread
 
 
-        public void flush(int lsn) {
-            if (lsn >= lastSavedLSN)
-                flush();
-        }
-        // lastSavedLSNは何
-
-        public Iterator<byte[]> iterator() {
+    public void flush(int lsn) {
+        if (lsn >= lastSavedLSN)
             flush();
-            return new LogIterator(fm, currentblk);
-        }
+    }
+    // lastSavedLSNは何
 
-        public synchronized int append(byte[] logrec) {
-            int boundary = logpage.getInt(0);
-            int recsize = logrec.length;
-            int bytesneeded = recsize + Integer.BYTES;
-            if (boundary - bytesneeded < Integer.BYTES) {
-                flush();
-                currentblk = appendNewBlock();
-                boundary = logpage.getInt(0);
-            }
-            int recpos = boundary - bytesneeded;
-            logpage.setBytes(recpos, logrec);
-            logpage.setInt(0, recpos); // 新boundary
-            latestLSN += 1;
-            return latestLSN;
-        }
-
-        private BlockId appendNewBlock() {
-            BlockId blk = fm.append(logfile);
-            logpage.setInt(0, fm.blockSize());
-            fm.write(blk, logpage);
-            return blk;
-        }
-
-        private void flush() {
-            fm.write(currentblk, logpage);
-            lastSavedLSN = latestLSN;
-        }
+    public Iterator<byte[]> iterator() {
+        flush();
+        return new LogIterator(fm, currentblk);
     }
 
+    public synchronized int append(byte[] logrec) {
+        int boundary = logpage.getInt(0);
+        int recsize = logrec.length;
+        int bytesneeded = recsize + Integer.BYTES;
+        if (boundary - bytesneeded < Integer.BYTES) {
+            flush();
+            currentblk = appendNewBlock();
+            boundary = logpage.getInt(0);
+        }
+        int recpos = boundary - bytesneeded;
+        logpage.setBytes(recpos, logrec);
+        logpage.setInt(0, recpos); // 新boundary
+        latestLSN += 1;
+        return latestLSN;
+    }
+
+    private BlockId appendNewBlock() {
+        BlockId blk = fm.append(logfile);
+        logpage.setInt(0, fm.blockSize());
+        fm.write(blk, logpage);
+        return blk;
+    }
+
+    private void flush() {
+        fm.write(currentblk, logpage);
+        lastSavedLSN = latestLSN;
+    }
 }
