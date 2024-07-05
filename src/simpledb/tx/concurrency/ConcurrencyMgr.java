e4d66543e1d3b615d18a20c9840b3199180a0a49
Takashi-Iskw <takashi.ishikawa.24@gmail.com>
2024-07-06 00:50:49 +0900

    ConcurrencyMgrを追加。 ・waitの処理がよく分からない。一回チャレンジして、既にLock取られていたら一旦待つべきだと思うが、その処理が上手くされているのか疑問。

---
+++ src/simpledb/tx/concurrency/ConcurrencyMgr.java
@@ -0,0 +1,39 @@
+package simpledb.tx.concurrency;
+
+import simpledb.file.BlockId;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConcurrencyMgr {
+    private static LockTable locktbl = new LockTable();
+    private Map<BlockId, String> locks = new HashMap<BlockId, String>();
+
+    public void sLock(BlockId blk) {
+        if(locks.get(blk) == null) {            // 対象blkが S にも X にもなってないならsLockを取得
+            locktbl.sLock(blk);
+            locks.put(blk, "S");
+        }
+    }
+    // 一回チャレンジしてダメだったら待つ、ということについて
+    // LockTableの方にwaitの処理があったが、ここでifの内側にあるのでは意味ないのでは？
+
+    public void xLock(BlockId blk) {
+        if(!hasXLock(blk)) {
+            sLock(blk);
+            locktbl.xLock(blk);
+            locks.put(blk, "X");
+        }
+    }
+
+    public void release() {
+        for(BlockId blk : locks.keySet())
+            locktbl.unlock(blk);                    // lockの個数-- lockが0になるならMapから削除
+        locks.clear();
+    }
+
+    private boolean hasXLock(BlockId blk) {
+        String locktype = locks.get(blk);
+        return locktype != null && locktype.equals("X");
+    }
+}
